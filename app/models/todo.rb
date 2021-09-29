bac7dcbeab4be3f7676694d84a7df73ba1ef09cd
rdelhibabu <rdelhibabu@gmail.com>
2021-09-29 15:58:47 +0530

    ready for submission L6

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,57 @@
+
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where(due_date: Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    where(completed: true)
+  end
+
+  def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    "#{id}. #{display_status} #{todo_text} #{due_date.to_s(:long)}"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.show_list
+    puts "My Todo list\n\n"
+
+    puts "OverDue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.add_task(todo)
+    todo_text = todo[:todo_text]
+    due_date = Date.today + todo[:due_in_days]
+    create!(todo_text: todo_text, due_date: due_date, completed: false)
+  end
+
+  def self.mark_as_complete(todo_id)
+    todo_for_completion = find(todo_id)
+    todo_for_completion.completed = true
+    todo_for_completion.save
+    return todo_for_completion
   end
 end
