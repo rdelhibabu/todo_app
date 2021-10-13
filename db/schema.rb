ae680202ac84d07a36da33d4bcfca32267c1d3aa
rdelhibabu <rdelhibabu@gmail.com>
2021-10-13 17:41:26 +0530

    commit schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_09_13_114149) do
     t.text "todo_text"
     t.date "due_date"
     t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
