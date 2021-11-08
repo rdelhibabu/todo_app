c2e82dac14f07d953cd6bb0e24bc9837c302d969
rdelhibabu <rdelhibabu@gmail.com>
2021-11-08 15:54:32 +0530

    L9 complete

--- db/migrate/20211102042323_add_password_digest_to_users.rb
+++ db/migrate/20211102042323_add_password_digest_to_users.rb
@@ -1,9 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[6.1]
   def change
-    change_table(:users) do |t|
-      t.string :password_digest
-    end
-
-      #add_column :users, :password_digest, :string
+    add_column :users, :password_digest, :string
   end
 end
