1aee30c2fe4f4e0bc54baf72d4e98ca29a431d75
rdelhibabu <rdelhibabu@gmail.com>
2021-11-08 16:48:08 +0530

    L9 complete

--- db/schema.rb
+++ db/schema.rb
@@ -15,22 +15,22 @@ ActiveRecord::Schema.define(version: 2021_09_13_114149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # create_table "todos", force: :cascade do |t|
-  #   t.text "todo_text"
-  #   t.date "due_date"
-  #   t.boolean "completed"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  #   t.bigint "user_id"
-  # end
-  #
-  # create_table "users", force: :cascade do |t|
-  #   t.string "email"
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  #   t.string "password_digest"
-  #   t.string "first_name"
-  #   t.string "last_name"
-  # end
+  create_table "todos", force: :cascade do |t|
+    t.text "todo_text"
+    t.date "due_date"
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+  end
 
 end
